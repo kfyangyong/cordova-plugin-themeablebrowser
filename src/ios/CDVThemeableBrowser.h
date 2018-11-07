b4a094d8868569b92e6c2d3ff1089085f34f9b3d
ayong <2238899650@qq.com>
2018-11-07 10:49:16 +0800

    change ios delegate

--- src/ios/CDVThemeableBrowser.h
+++ src/ios/CDVThemeableBrowser.h
@@ -28,7 +28,19 @@
     #import <Cordova/CDVWebViewDelegate.h>
 #endif
 
-#import "CDVThemeableBrowserUIDelegate.h"
+#import <WebKit/WebKit.h>
+
+@interface CDVThemeableBrowserUIDelegate : NSObject <WKUIDelegate>{
+@private
+    UIViewController* _viewController;
+}
+
+@property (nonatomic, copy) NSString* title;
+
+- (instancetype)initWithTitle:(NSString*)title;
+-(void) setViewController:(UIViewController*) viewController;
+
+@end
 
 @interface CDVThemeableBrowserOptions : NSObject {}
 
--- src/ios/CDVThemeableBrowser.m
+++ src/ios/CDVThemeableBrowser.m
@@ -62,6 +62,107 @@
 
 #pragma mark CDVThemeableBrowser
 
+@implementation CDVThemeableBrowserUIDelegate
+
+- (instancetype)initWithTitle:(NSString*)title{
+    self = [super init];
+    if (self) {
+        self.title = title;
+    }
+    
+    return self;
+}
+
+- (void)webView:(WKWebView*)webView runJavaScriptAlertPanelWithMessage:(NSString*)message
+initiatedByFrame:(WKFrameInfo*)frame completionHandler:(void (^)(void))completionHandler{
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:self.title
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* ok = [UIAlertAction actionWithTitle:NSLocalizedString(@"OK", @"OK")
+                                                 style:UIAlertActionStyleDefault
+                                               handler:^(UIAlertAction* action)
+                         {
+                             completionHandler();
+                             [alert dismissViewControllerAnimated:YES completion:nil];
+                         }];
+    
+    [alert addAction:ok];
+    
+    [[self getViewController] presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)webView:(WKWebView*)webView runJavaScriptConfirmPanelWithMessage:(NSString*)message
+initiatedByFrame:(WKFrameInfo*)frame completionHandler:(void (^)(BOOL result))completionHandler{
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:self.title
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* ok = [UIAlertAction actionWithTitle:NSLocalizedString(@"OK", @"OK")
+                                                 style:UIAlertActionStyleDefault
+                                               handler:^(UIAlertAction* action)
+                         {
+                             completionHandler(YES);
+                             [alert dismissViewControllerAnimated:YES completion:nil];
+                         }];
+    
+    [alert addAction:ok];
+    
+    UIAlertAction* cancel = [UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", @"Cancel")
+                                                     style:UIAlertActionStyleDefault
+                                                   handler:^(UIAlertAction* action)
+                             {
+                                 completionHandler(NO);
+                                 [alert dismissViewControllerAnimated:YES completion:nil];
+                             }];
+    [alert addAction:cancel];
+    
+    [[self getViewController] presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)webView:(WKWebView*)webView runJavaScriptTextInputPanelWithPrompt:(NSString*)prompt
+    defaultText:(NSString*)defaultText initiatedByFrame:(WKFrameInfo*)frame
+completionHandler:(void (^)(NSString* result))completionHandler{
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:self.title
+                                                                   message:prompt
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* ok = [UIAlertAction actionWithTitle:NSLocalizedString(@"OK", @"OK")
+                                                 style:UIAlertActionStyleDefault
+                                               handler:^(UIAlertAction* action)
+                         {
+                             completionHandler(((UITextField*)alert.textFields[0]).text);
+                             [alert dismissViewControllerAnimated:YES completion:nil];
+                         }];
+    
+    [alert addAction:ok];
+    
+    UIAlertAction* cancel = [UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", @"Cancel")
+                                                     style:UIAlertActionStyleDefault
+                                                   handler:^(UIAlertAction* action)
+                             {
+                                 completionHandler(nil);
+                                 [alert dismissViewControllerAnimated:YES completion:nil];
+                             }];
+    [alert addAction:cancel];
+    
+    [alert addTextFieldWithConfigurationHandler:^(UITextField* textField) {
+        textField.text = defaultText;
+    }];
+    
+    [[self getViewController] presentViewController:alert animated:YES completion:nil];
+}
+
+-(UIViewController*) getViewController{
+    return _viewController;
+}
+
+-(void) setViewController:(UIViewController*) viewController{
+    _viewController = viewController;
+}
+
+@end
+
 @interface CDVThemeableBrowser () {
     BOOL _isShown;
     int _framesOpened;  // number of frames opened since the last time browser exited
